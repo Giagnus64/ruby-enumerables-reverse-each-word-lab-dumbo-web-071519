c12e925266397f78db5e319f3b2dc2451ccbedbf
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2019-06-12 20:45:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,12 @@
+=begin # using .each
 def reverse_each_word(string)
   array = string.split(" ");
   new_array = []
   array.each {|word| new_array.push(word.reverse!)}
   new_array.join(" ")
+end
+=end
+def reverse_each_word(string)
+  array = string.split(" ");
+  new_array = array.each {|word| word.reverse!}
 end
